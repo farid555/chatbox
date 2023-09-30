ef864e10b32ab495a728fafb14147a631ff00138
Mohammad Farid Uzzaman <60000543+farid555@users.noreply.github.com>
2023-09-30 21:23:07 +0300

    typescript-transform-paths ...updated the paths @root/...

--- src/setupDatabase.ts
+++ src/setupDatabase.ts
@@ -1,5 +1,5 @@
-import mongoose, { disconnect } from 'mongoose';
-import { config } from './config';
+import mongoose from 'mongoose';
+import { config } from '@root/config';
 import Logger from 'bunyan';
 
 const log: Logger = config.createLogger('setupDatabase');
--- src/setupServer.ts
+++ src/setupServer.ts
@@ -14,16 +14,13 @@ import cookieSession from 'cookie-session';
 import HTTP_STATUS from 'http-status-codes';
 import 'express-async-errors';
 import compression from 'compression';
-import { config } from './config';
+import { config } from '@root/config';
 import { Server } from 'socket.io';
 import { createClient } from 'redis';
 import { createAdapter } from '@socket.io/redis-adapter';
-import applicationRoutes from './routes';
+import applicationRoutes from '@root/routes';
 import Logger from 'bunyan';
-import {
-  CustomError,
-  IErrorResponse,
-} from './shared/globals/helpers/error-handler';
+import {CustomError, IErrorResponse} from '@global/helpers/error-handler';
 
 const SERVER_PORT = 6000;
 const log: Logger = config.createLogger('server');
